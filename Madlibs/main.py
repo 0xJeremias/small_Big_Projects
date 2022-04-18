173944b2e8e994b2179e15a2492f58dd60681543
Jere <jeremiasblanco@gmail.com>
2022-04-17 21:27:00 -0300

    First commit

---
+++ Madlibs/main.py
@@ -0,0 +1,12 @@
+
+adj1= input("Adjetive: ")
+adj2= input("Adjetive: ")
+name= input("Your name: ")
+
+
+
+madlib = f"The world is indeed full of {adj1}, and in it there are many {adj2} places; \
+but still there is much that is fair, and though in all lands love is now \
+mingled with grief, it grows perhaps the greater. \
+— {name} in The Fellowship of the Ring, 'Lothlórien'"
+print(madlib)
